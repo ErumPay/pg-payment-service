0f65ecc5448b01415950a749817b98918dbacec9
0chotona <eunjongh97@gmail.com>
2026-05-14 15:10:49 +0900

    [KAN-1099] chore: 초기 세팅

---
+++ src/main/java/com/erumpay/pgpayment/PgpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.erumpay.pgpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erumpay/pgpayment/PgpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erumpay.pgpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
